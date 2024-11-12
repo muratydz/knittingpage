23ed4645910c600583a892b115f61cdedbb40907
Murat Yıldız <20muratyildiz00@gmail.com>
2024-11-13 00:51:37 +0300

    fix-product-details

--- src/component/ProductAbout/ProductAbout.jsx
+++ src/component/ProductAbout/ProductAbout.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import { getAllKnitting } from '../../redux/slices/apiSlicer';
-import "../../css/AllProduct.css"
+import "../../css/ProductAbout.css"
+
+
 
 function ProductAbout() {
 
@@ -14,20 +16,41 @@ function ProductAbout() {
 
     useEffect(() => {
         dispatch(getAllKnitting());
-    }, [])
+    }, [dispatch])
+
+    const product = knitting?.find(item => item.id === id);
+
 
     return (
-        <div className='product-about'>
-            {knitting && knitting.map((item) => (
-                item.id == id ? (
-                    <div key={item.id}>
-                        <img src={item.img} alt={item.header} />
-                        <h2>{item.header}</h2>
-                        <p>{item.description}</p>
-
+        <div className='product-about container'>
+            {product ? (
+                <div key={product.id} className='product-main'>
+                    <div>
+                        <img src={product.img} alt={product.header} />
                     </div>
-                ) : null
-            ))}
+                    <div>
+                        <h2>{product.header}</h2>
+                        <p>{product.description}</p>
+                    </div>
+                </div>
+            ) : (
+                <p>Ürün bulunamadı</p>
+            )}
+            {product ? (
+                <div>
+                    <h2>PRODUCT DETAİLS</h2>
+                    <div className='product-details'>
+                        <div>
+                            <p>{product.details}</p>
+                        </div>
+                        <div className='product-slider'>
+                        </div>
+                    </div>
+                </div>
+            ) :
+                (
+                    null
+                )}
         </div>
     )
 }
